7e81632cb737bf9bcba0e0c3fd9e41a9cb8f8606
K. Johnson <code@afrodave.net>
2015-05-17 16:37:39 +0100

    vimrc
    - few new plugins

--- shell/zsh/bind.zsh
+++ shell/zsh/bind.zsh
@@ -1,8 +1,6 @@
 #! /usr/bin/env zsh
 
-bindkey '^A' beginning-of-line
-bindkey '^E' end-of-line
+bindkey -e
+
 bindkey "^[[C" forward-word
 bindkey "^[[D" backward-word
-bindkey "^R" history-incremental-search-backward
-bindkey "^P" history-search-backward
